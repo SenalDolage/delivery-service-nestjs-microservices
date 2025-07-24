ed798a4803ce811d8a7851f3dcdd8a7c3558807b
SenalDolage <senal.dolage@gmail.com>
2025-07-24 16:31:43 +0800

    saving rider coordinates and validating request

--- apps/logging/src/rider-coordinates/rider-coordinates.controller.ts
+++ apps/logging/src/rider-coordinates/rider-coordinates.controller.ts
@@ -1,9 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateCoordinatesDTO } from './dto/create-coordinates.dto';
+import { RiderCoordinatesService } from './rider-coordinates.service';
 
 @Controller('rider-coordinates')
 export class RiderCoordinatesController {
+  constructor(private coordinatsService: RiderCoordinatesService) {}
+
   @Get()
   getRiderCoordinates() {
     return 'Hellow i am from rider coordinates';
+  }
+
+  @Post()
+  saveRiderCoordinates(
+    @Body()
+    createCoordinateDTO: CreateCoordinatesDTO,
+  ) {
+    return this.coordinatsService.saveRiderCoordiantes(createCoordinateDTO);
   }
 }
--- apps/logging/src/rider-coordinates/rider-coordinates.service.ts
+++ apps/logging/src/rider-coordinates/rider-coordinates.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { CreateCoordinatesDTO } from './dto/create-coordinates.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { RiderCoordinate } from './schemas/rider-coordinates.schema';
 
 @Injectable()
-export class RiderCoordinatesService {}
+export class RiderCoordinatesService {
+  constructor(
+    @InjectModel(RiderCoordinate.name)
+    private readonly riderCoodinateModel: Model<RiderCoordinate>,
+  ) {}
+  async saveRiderCoordiantes(createCoordinateDTO: CreateCoordinatesDTO) {
+    return await this.riderCoodinateModel.create(createCoordinateDTO);
+  }
+}
